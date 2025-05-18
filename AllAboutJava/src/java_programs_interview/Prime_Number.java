49c7e030f956debc9a4d34322f1d758770bef9d6
TestWithVivek <vivekvardhan2706@gmail.com>
2025-05-18 12:50:30 +0530

    prime number logic change

--- AllAboutJava/src/java_programs_interview/Prime_Number.java
+++ AllAboutJava/src/java_programs_interview/Prime_Number.java
@@ -1,40 +1,43 @@
 package java_programs_interview;
 
+import java.util.Scanner;
+
 public class Prime_Number {
 
-	
 	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+
+        int num = sc.nextInt();
+        
+        int i;
+        
+        int temp=0;
+
+       for(i=2; i<num; i++) {
+    	   
+    	   if(num%i==0) {
+    		   
+    		   temp++;
+    		   
+    		   
+    	   }
+    	   
+       }
+       
+       if(temp==0) {
+    	   
+    	   System.out.println("The given numnber is Prime number");
+    	   
+       }else {
+    	   
+    	   System.out.println("The given number is not Prime nnumber");
+       }
+
+      
+    }
 		
-		// Prime Number: Prime number is the number which is divisible by 1 and the number itslef 
-		//e.g. 2, 3 , 5 , 7, 11, 13
-		 
-		
-		int number = 10;
-		
-		int count=0;
-		
-		for(int i=1;i<=number;i++) {
-			
-			
-			if(number%i==0) {
-				
-				count++;
-				
-			}
-			
-		}
 	
-		if (count == 2) {
-			
-            System.out.println(number + " is a prime number.");
-            
-        } else {
-        	
-            System.out.println(number + " is not a prime number.");
-        }
-		
-		
-		
-		
-	}
 }
